6828a9b790a4ad587ccd3daa7e1a86810a84d5b8
lithath <35553115+lithath@users.noreply.github.com>
2018-01-18 10:24:45 +0530

    Update user.java

--- user.java
+++ user.java
@@ -2,9 +2,12 @@ public class User{
 
 public static void main(String args[])
 {
-
+ System.out.println("From branch1:  ");  
 System.out.println("Welcome User:  ");
 System.out.println("Please enter details:  ");
+    
+    
+ System.out.println("From branch2:  ");   
     
 
 }}
